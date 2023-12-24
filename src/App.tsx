6c899f787ca896a96d626c64099817ec3a3a9d66
정언용 <unjoo94@naver.com>
2023-12-24 17:59:52 +0900

    index && App file edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.module.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Header from "./components/Header/Header";
