5a6062ac0914e3258ecfc93d770c54b32402d0dc
정언용 <unjoo94@naver.com>
2023-12-24 14:52:33 +0900

    docker-compose up && react Header and Footer add

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="Header">
+      <p>This is Header</p>
+      <p>Tihs is Menu</p>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <p className="Home">
+      <p className="Title">Home Page</p>
+    </p>
+  );
+};
+
+export default Home;
