810deff69e39fdd17d36c5ea7048c389731efeb4
정언용 <unjoo94@naver.com>
2024-01-07 17:21:08 +0900

    :books: : naver map API Marker

--- app/src/pages/Map/Map.tsx
+++ app/src/pages/Map/Map.tsx
@@ -3,10 +3,20 @@ import styles from "./Map.module.scss";
 
 const Map = () => {
   useEffect(() => {
-    const mapDiv = document.getElementById("map");
-    if (mapDiv) {
-      const map = new window.naver.maps.Map(mapDiv);
-    }
+    const NaverMap = naver.maps;
+    let position = new NaverMap.LatLng(37.3595704, 127.105399);
+    const map = new NaverMap.Map("map", {
+      center: position,
+    });
+
+    const marker = new NaverMap.Marker({
+      position: position,
+      map: map,
+    });
+
+    naver.maps.Event.addListener(map, "click", (e) =>
+      marker.setPosition(e.coord)
+    );
   }, []);
 
   return <div id="map" className={styles.Map}></div>;
