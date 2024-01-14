16a06c5a3025ee521f51f3040007f3e81c9e61c5
정언용 <unjoo94@naver.com>
2024-01-14 14:44:34 +0900

    현재 위치를 받아오는 함수 적용

--- app/src/pages/Map/Map.tsx
+++ app/src/pages/Map/Map.tsx
@@ -4,23 +4,27 @@ import styles from "./Map.module.scss";
 const Map = () => {
   useEffect(() => {
     const NaverMap = naver.maps;
-    let position: naver.maps.LatLng = new NaverMap.LatLng(
-      37.3595704,
-      127.105399
-    );
+    // 현재 위치를 허용하여 위치를 받아오는 함수
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const { latitude, longitude } = pos.coords;
+      const currPos = new NaverMap.LatLng(latitude, longitude);
+      const map: naver.maps.Map = new NaverMap.Map("map", {
+        center: currPos,
+        zoomControl: true,
+        zoomControlOptions: {
+          position: naver.maps.Position.TOP_RIGHT,
+        },
+      });
 
-    const map: naver.maps.Map = new NaverMap.Map("map", {
-      center: position,
+      const marker: naver.maps.Marker | null = new NaverMap.Marker({
+        position: currPos,
+        map: map,
+      });
+
+      naver.maps.Event.addListener(map, "click", (e) =>
+        marker.setPosition(e.coord)
+      );
     });
-
-    const marker: naver.maps.Marker = new NaverMap.Marker({
-      position: position,
-      map: map,
-    });
-
-    naver.maps.Event.addListener(map, "click", (e) =>
-      marker.setPosition(e.coord)
-    );
   }, []);
 
   return <div id="map" className={styles.Map}></div>;
